b3473430f71751d3796c8f82d926dc46611ea0d1
Santiago Carretto <santiagocarretto@live.com>
2021-09-08 20:59:19 -0300

    “v8.3”

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,12 +1,10 @@
 import Class from './Class';
 import Vino from './AngelicaZapataMalbec.jpg';
 
-const ItemListCointainer = () => {
-    return (
-      
-<Class Style="display: flex;" Vino={Vino} item='Angelica Zapata Alta Malbec'  valor='$2.850'/>
-
-    )
-  };
+function ItemListCointainer () {
+    return <>
+      <Class Style="display: flex;" Vino={Vino} item='Angelica Zapata Alta Malbec'  valor='$2.850'/>
+</>;
+  }
   
   export default ItemListCointainer;
